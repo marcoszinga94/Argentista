7a2a53be231a8667d9813e779cda4662b7517935
Marcos Federico Zingaretti <marcoszinga94@gmail.com>
2025-01-30 12:07:54 -0300

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server'
-  // adapter: vercelServerless({
-  //   webAnalytics: {
-  //     enabled: true,
-  //   },
-  // }),
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
