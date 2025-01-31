fc93624f080cf20f69c25ce8c5032710d914287c
Marcos Federico Zingaretti <marcoszinga94@gmail.com>
2025-01-31 20:54:54 -0300

    fix chevron

--- src/components/InflationChart.jsx
+++ src/components/InflationChart.jsx
@@ -101,9 +101,14 @@ const InflationChart = () => {
             >
               <span>{decade}</span>
               {openItems[decade] ? (
-                <img src="src/Icons/chevrondown.svg" alt="Chevron down" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="32" width="32">
+                <path
+                  d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+              </svg>
               ) : (
-                <img src="src/Icons/chevronright.svg" alt="Chevron right" />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" height="32" width="32">
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+                </svg>
               )}
             </button>
             {openItems[decade] && (
@@ -127,15 +132,15 @@ const InflationChart = () => {
                             {(data.total / data.count).toFixed(1)}%
                           </span>
                           {openItems[`${decade}-${year}`] ? (
-                            <img
-                              src="src/Icons/chevrondown.svg"
-                              alt="Chevron down"
-                            />
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="32" width="32">
+                            <path
+                              d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                          </svg>
                           ) : (
-                            <img
-                              src="src/Icons/chevronright.svg"
-                              alt="Chevron right"
-                            />
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" height="32" width="32">
+  <path
+    d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+</svg>
                           )}
                         </div>
                       </button>
