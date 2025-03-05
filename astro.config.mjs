47eea77877bb500f3594341945dd230bf8ca9bf8
marcoszinga94 <marcoszinga94@gmail.com>
2025-03-05 14:27:55 -0300

    a ver ahora?

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
