02d4e524d1cfc4721f388b9eb1f5148ecb719442
marcoszinga94 <marcoszinga94@gmail.com>
2025-03-05 15:00:22 -0300

    a ver ahora plis!

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
   integrations: [react()],
   output: 'server',
   adapter: vercel(),
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwind()]
   }
 });
