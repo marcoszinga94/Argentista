4ca97e851805315ffaed572e150d77dd9e1569ec
marcoszinga94 <marcoszinga94@gmail.com>
2025-03-05 11:57:53 -0300

    update tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [react()],
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				azulClaro: '#EFF6FF',
+				azul: '#3B82F6',
+				azulOscuro: '#1E40AF',
+				grisClaro: '#F9FAFB',
+				gris: '#6B7280',
+				grisOscuro: '#374151',
+				rojo: '#EF4444',
+			},
+		},
 	},
 	plugins: [],
+	future: {
+		hoverOnlyWhenSupported: true,
+		respectDefaultRingColorOpacity: true,
+		disableColorOpacityUtilitiesByDefault: true,
+	},
+	experimental: {
+		optimizeUniversalDefaults: true,
+	},
 }
